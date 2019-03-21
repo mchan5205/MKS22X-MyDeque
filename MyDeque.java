954c4fd35082eeea40c6b8a86b00e378a634ed24
Michael Chan <mchan10@stuy.edu>
2019-03-21 11:03:41 -0400

    added constructors

---
+++ MyDeque.java
@@ -0,0 +1,23 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+  @SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+  public MyDeque(int capacity){
+    data = (E[])new Object[capacity];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+  public int size(){
+    return size;
+  }
+  public void addFirst(E element){
+
+  }
+}
