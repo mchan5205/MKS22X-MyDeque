d995962f2d7825b9533802d0df437ffe17b6abc2
Michael Chan <mchan10@stuy.edu>
2019-03-26 11:01:18 -0400

    added scanner and eval method

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Calculator{
+  public static double eval(String s){
+    Scanner t = new Scanner(s);
+    System.out.println(t.next());
+    System.out.println(t.next());
+    System.out.println(t.next());
+    return (0.0);
+  }
+  public static void main(String[] args){
+    eval("1 2 +");
+  }
+}
